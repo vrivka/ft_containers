2da2ec4b689c3776f2037b652933451a7c44fe4e
vrivka <vladkolezov@gmail.com>
2021-10-23 13:05:48 +0300

    vector done

--- ft_vector.hpp
+++ ft_vector.hpp
@@ -6,21 +6,22 @@
 #include "ft_iterator.hpp"
 #include "ft_reverse_iterator.hpp"
 
-template <class T, class Alloc = std::allocator<T> >
+namespace ft{
+template<class T, class Alloc = std::allocator<T> >
 class vector {
 public:
-	typedef T											value_type;				//	The first template parameter (T)
-	typedef Alloc										allocator_type;			//	The second template parameter (Alloc)	defaults to: allocator<value_type>
-	typedef typename allocator_type::reference			reference ;				//	for the default allocator: value_type&
-	typedef typename allocator_type::const_reference 	const_reference;		//	for the default allocator: const value_type&
-	typedef typename allocator_type::pointer			pointer;				//	for the default allocator: value_type*
-	typedef typename allocator_type::const_pointer		const_pointer;			//	for the default allocator: const value_type*
-	typedef ft::iterator<pointer>						iterator;				//	a random access iterator to value_type	convertible to const_iterator
-	typedef ft::iterator<const_pointer>					const_iterator;			//	a random access iterator to const value_type
-	typedef ft::reverse_iterator<iterator>				reverse_iterator;		//	reverse_iterator<iterator>
-	typedef ft::reverse_iterator<const_iterator>			const_reverse_iterator;	//	reverse_iterator<const_iterator>
-	typedef ptrdiff_t									difference_type;		//	a signed integral type, identical to: iterator_traits<iterator>::difference_type	usually the same as ptrdiff_t
-	typedef size_t										size_type;				//	an unsigned integral type that can represent any non-negative value of difference_type	usually the same as size_t
+	typedef T value_type;                //	The first template parameter (T)
+	typedef Alloc allocator_type;            //	The second template parameter (Alloc)	defaults to: allocator<value_type>
+	typedef typename allocator_type::reference reference;                //	for the default allocator: value_type&
+	typedef typename allocator_type::const_reference const_reference;        //	for the default allocator: const value_type&
+	typedef typename allocator_type::pointer pointer;                //	for the default allocator: value_type*
+	typedef typename allocator_type::const_pointer const_pointer;            //	for the default allocator: const value_type*
+	typedef ft::iterator<pointer> iterator;                //	a random access iterator to value_type	convertible to const_iterator
+	typedef ft::iterator<const_pointer> const_iterator;            //	a random access iterator to const value_type
+	typedef ft::reverse_iterator<iterator> reverse_iterator;        //	reverse_iterator<iterator>
+	typedef ft::reverse_iterator<const_iterator> const_reverse_iterator;    //	reverse_iterator<const_iterator>
+	typedef ptrdiff_t difference_type;        //	a signed integral type, identical to: iterator_traits<iterator>::difference_type	usually the same as ptrdiff_t
+	typedef size_t size_type;                //	an unsigned integral type that can represent any non-negative value of difference_type	usually the same as size_t
 private:
 	allocator_type A;
 	size_type len;
@@ -30,27 +31,34 @@ public:
 	/* Iterators */
 
 	iterator begin() { return arr; };
+
 	const_iterator begin() const { return arr; }; //Return iterator to beginning (public member function )
 
 	iterator end() { return arr + len; };
+
 	const_iterator end() const { return arr + len; }; //Return iterator to end (public member function )
 
 	reverse_iterator rbegin() { return end() - 1; };
-	const_reverse_iterator rbegin() const { return end() - 1; }; //Return reverse iterator to reverse beginning (public member function )
+
+	const_reverse_iterator rbegin() const {
+		return end() - 1;
+	}; //Return reverse iterator to reverse beginning (public member function )
 
 	reverse_iterator rend() { return begin() - 1; };
-	const_reverse_iterator rend() const { return begin() - 1; }; //Return reverse iterator to reverse end (public member function )
+
+	const_reverse_iterator rend() const {
+		return begin() - 1;
+	}; //Return reverse iterator to reverse end (public member function )
 
 	/* Capacity */
 
-	size_type size() const { return len; };									//	Return size (public member function )
-	size_type max_size() const { return A.max_size(); };			//	Return maximum size (public member function )
+	size_type size() const { return len; };                                    //	Return size (public member function )
+	size_type max_size() const { return A.max_size(); };            //	Return maximum size (public member function )
 	void resize(size_type n, value_type const &val = value_type()) {
 		if (n < len) {
 			for (size_type i = n; i < len; i++)
 				A.destroy(arr + i);
-		}
-		else if (n > len) {
+		} else if (n > len) {
 			if (n > cap)
 				cap = n > cap * 2 ? n : cap * 2;
 			pointer new_arr = A.allocate(cap);
@@ -62,18 +70,20 @@ public:
 				A.destroy(arr + i);
 			A.deallocate(arr, cap);
 			arr = new_arr;
-		}
-		else
-			return ;
+		} else
+			return;
 		len = n;
-	};	//	Change size (public member function )
-	size_type capacity() const { return cap; };								//	Return size of allocated storage capacity (public member function )
-	bool empty() const { return len == 0; };								//	Test whether vector is empty (public member function )
+	};    //	Change size (public member function )
+	size_type
+	capacity() const { return cap; };                                //	Return size of allocated storage capacity (public member function )
+	bool empty() const {
+		return len == 0;
+	};                                //	Test whether vector is empty (public member function )
 	void reserve(size_type n) {
 		if (n > cap)
 			cap = n;
 		else
-			return ;
+			return;
 		pointer new_arr = A.allocate(cap);
 		for (size_type i = 0; i < len; i++)
 			A.construct(new_arr + i, arr[i]);
@@ -81,33 +91,40 @@ public:
 			A.destroy(arr + i);
 		A.deallocate(arr, cap);
 		arr = new_arr;
-	};										//	Request a change in capacity (public member function )
+	};                                        //	Request a change in capacity (public member function )
 
 	/* Element access */
 
 	reference operator[](size_type n) { return arr[n]; };
-	const_reference operator[](size_type n) const { return arr[n]; };		//	Access element (public member function )
+
+	const_reference operator[](size_type n) const { return arr[n]; };        //	Access element (public member function )
 
 	reference at(size_type n) {
 		if (n >= len)
 			throw std::out_of_range("vector");
 		return arr[n];
 	};
+
 	const_reference at(size_type n) const {
 		if (n >= len)
 			throw std::out_of_range("vector");
 		return arr[n];
-	};							//	Access element (public member function )
+	};                            //	Access element (public member function )
 
 	reference front() { return arr[0]; };
-	const_reference front() const { return arr[0]; };						//	Access first element (public member function )
+
+	const_reference
+	front() const { return arr[0]; };                        //	Access first element (public member function )
 
 	reference back() { return arr[len - 1]; };
-	const_reference back() const { return arr[len - 1]; };					//	Access last element (public member function )
+
+	const_reference back() const {
+		return arr[len - 1];
+	};                    //	Access last element (public member function )
 
 	/* Modifiers */
 
-	template <class InputIterator>
+	template<class InputIterator>
 	void assign(InputIterator first, InputIterator last) {
 		size_type n = 0;
 		for (; first + n != last; n++);
@@ -120,7 +137,8 @@ public:
 		for (size_type i = 0; first + i != last; i++)
 			A.construct(arr + i, *(first + i));
 	};
-	void assign(size_type n, const value_type& val) {
+
+	void assign(size_type n, const value_type &val) {
 		cap = n > cap ? n : cap;
 		for (size_type i = 0; i < len; i++)
 			A.destroy(arr + i);
@@ -129,9 +147,9 @@ public:
 		arr = A.allocate(cap);
 		for (size_type i = 0; i < len; i++)
 			A.construct(arr + i, val);
-	};					//	Assign vector content (public member function )
+	};                    //	Assign vector content (public member function )
 
-	void push_back(const value_type& val) {
+	void push_back(const value_type &val) {
 		if (cap == 0) {
 			A.deallocate(arr, cap);
 			cap = 1;
@@ -150,83 +168,81 @@ public:
 				A.destroy(arr + i);
 			A.deallocate(arr, old_cap);
 			arr = new_arr;
-		}
-		else
+		} else
 			A.construct(&arr[len], val);
 		len += 1;
-	};							//	Add element at the end (public member function )
+	};                            //	Add element at the end (public member function )
 	void pop_back() {
 		A.destroy(arr + (len - 1));
 		len -= 1;
-	};													//	Delete last element (public member function )
+	};                                                    //	Delete last element (public member function )
 
-	iterator insert(iterator position, const value_type& val) {
+	iterator insert(iterator position, const value_type &val) {
+		typename iterator::difference_type t = position - begin();
+
 		if (cap == 0) {
-			typename iterator::difference_type t = position - begin();
-
 			A.deallocate(arr, cap);
 			cap = 1;
 			arr = A.allocate(cap);
 			A.construct(arr + t, val);
-		}
-		else if (cap == len) {
+		} else if (cap == len) {
 			size_type old_cap = cap;
+			size_type i;
+
 			cap *= 2;
 			pointer new_arr = A.allocate(cap);
 
-			for (size_type i = 0; arr + i != position; i++)
-				A.construct(new_arr + i, arr[i]);
-			A.construct(new_arr + (position - begin()), val);
-			for (size_type i = position - begin(); i < len; i++)
+			A.construct(new_arr + t, val);
+			for (i = t; i > 0; i--)
+				A.construct(new_arr + i - 1, arr[i - 1]);
+			for (i = t; i < len; i++)
 				A.construct(new_arr + i + 1, arr[i]);
-			for (size_type i = 0; i < len; i++)
-				A.destroy(arr + i);
+			for (i = len; i > 0; i--)
+				A.destroy(arr + i - 1);
 			A.deallocate(arr, old_cap);
 			arr = new_arr;
-		}
-		else if (position == end())
+		} else if (t == len)
 			A.construct(arr + len, val);
 		else {
 			size_type i = len - 1;
 
 			A.construct(arr + len, arr[i]);
-			for (; arr + i != position; i--)
+			for (; i != t; i--)
 				arr[i] = arr[i - 1];
 			arr[i] = val;
 		}
 		len++;
-		return arr + (position - begin());
+		return arr + t;
 	}; // single element (1)
 
-	void insert (iterator position, size_type n, const value_type& val) {
+	void insert(iterator position, size_type n, const value_type &val) {
 		typename iterator::difference_type t = position - begin();
+
 		if (cap == 0) {
-			std::cout << t << std::endl;
 			A.deallocate(arr, cap);
 			cap = n;
 			arr = A.allocate(cap);
 			for (size_type i = 0; i < n; i++)
 				A.construct(arr + t + i, val);
-		}
-		else if (cap == len || len + n > cap) {
+		} else if (cap == len || len + n > cap) {
 			size_type old_cap = cap;
 			cap = len + n > cap * 2 ? len + n : cap * 2;
 			pointer new_arr = A.allocate(cap);
-			size_type i = 0;
-			size_type ii = 0;
 
-			for (; arr + i != position; i++, ii++)
-				A.construct(new_arr + ii, arr[i]);
-			for (size_type k = 0; k < n; ii++, k++)
-				A.construct(new_arr + ii, val);
-			for (; i < len; ii++, i++)
-				A.construct(new_arr + ii, arr[i]);
-			for (i = 0; i < len; i++)
-				A.destroy(arr + i);
+			size_type i;
+			size_type k = t + n;
+
+			for (i = t; i < k; i++)
+				A.construct(new_arr + i, val);
+			for (i = t; i > 0; i--)
+				A.construct(new_arr + i - 1, arr[i - 1]);
+			for (i = t; i < len; i++)
+				A.construct(new_arr + i + n, arr[i]);
+			for (i = len; i > 0; i--)
+				A.destroy(arr + i - 1);
 			A.deallocate(arr, old_cap);
 			arr = new_arr;
-		}
-		else if (position == end())
+		} else if (t == len)
 			for (size_type i = 0; i < n; i++)
 				A.construct(arr + len + i, val);
 		else {
@@ -242,17 +258,74 @@ public:
 		len += n;
 	};// fill (2)
 
-	template <class InputIterator>
-	void insert (iterator position, InputIterator first, InputIterator last) {
+	template<class InputIterator>
+	void insert(iterator position, InputIterator first, InputIterator last) {
+		typename iterator::difference_type t = position - begin();
+		size_type n = 0;
 
+		for (; first + n != last; n++);
+
+		if (cap == 0) {
+			A.deallocate(arr, cap);
+			cap = n;
+			arr = A.allocate(cap);
+			for (size_type i = 0; i < n; i++)
+				A.construct(arr + t + i, first[i]);
+		} else if (cap == len || len + n > cap) {
+			size_type old_cap = cap;
+			cap = len + n > cap * 2 ? len + n : cap * 2;
+			pointer new_arr = A.allocate(cap);
+
+			size_type i;
+			size_type ii = 0;
+			size_type k = t + n;
+
+			for (i = t; i < k; i++, ii++)
+				A.construct(new_arr + i, first[ii]);
+			for (i = t; i > 0; i--)
+				A.construct(new_arr + i - 1, arr[i - 1]);
+			for (i = t; i < len; i++)
+				A.construct(new_arr + i + n, arr[i]);
+			for (i = len; i > 0; i--)
+				A.destroy(arr + i - 1);
+			A.deallocate(arr, old_cap);
+			arr = new_arr;
+		} else if (t == len)
+			for (size_type i = 0; i < n; i++)
+				A.construct(arr + len + i, first[i]);
+		else {
+			size_type i = len - 1;
+			size_type ii = 0;
+
+			for (size_type k = n; k > 0; k--, i--)
+				A.construct(arr + len + k - 1, arr[i]);
+			for (i++; i != t; i--)
+				arr[i + n - 1] = arr[i - 1];
+			for (size_type k = n; k > 0; k--, ii++)
+				arr[i + k - 1] = first[ii];
+		}
+		len += n;
 	};// Insert elements (public member function ) range (3)
 
-	iterator erase (iterator position) {
-
+	iterator erase(iterator position) {
+		for (size_type t = position - begin(); t < len - 1; t++)
+			arr[t] = arr[t + 1];
+		A.destroy(arr + len - 1);
+		len--;
+		return position;
 	};
 
-	iterator erase (iterator first, iterator last) {
+	iterator erase(iterator first, iterator last) {
+		size_type i = last - first;
+		size_type t = last - begin() - 1;
+		size_type k = first - begin();
 
+		for (; t < len - 1; t++, k++)
+			arr[k] = arr[t + 1];
+		for (size_type d = 0; d < i; d++)
+			A.destroy(arr + len - 1 - d);
+		len -= i;
+		return last;
 	}; //Erase elements (public member function )
 
 	void swap(vector &x) {
@@ -270,35 +343,41 @@ public:
 
 		cap = x.cap;
 		x.cap = tmp_cap;
-	};												//	Swap content (public member function )
+	};                                                //	Swap content (public member function )
 	void clear() {
-		for (size_type i = 0; i < len; i++)
-			A.destroy(arr + i);
+		for (size_type i = len; i > 0; i--)
+			A.destroy(arr + i - 1);
 		len = 0;
-	};														//	Clear content (public member function )
+	};                                                        //	Clear content (public member function )
 
 	/* Allocator */
 
-	allocator_type get_allocator() const { return A; };								//	Get allocator (public member function )
+	allocator_type
+	get_allocator() const { return A; };                                //	Get allocator (public member function )
 
 	/* Constructors */
 
-	explicit vector(const allocator_type& alloc = allocator_type()) : A(alloc), len(0), cap(0) {
+	explicit vector(const allocator_type &alloc = allocator_type()) : A(alloc), len(0), cap(0) {
 		arr = A.allocate(0);
 	};
-	explicit vector(size_type n, const value_type &val = value_type(), const allocator_type& alloc = allocator_type()) : A(alloc), len(n), cap(n) {
+
+	explicit vector(size_type n, const value_type &val = value_type(), const allocator_type &alloc = allocator_type())
+			: A(alloc), len(n), cap(n) {
 		arr = A.allocate(cap);
 		for (size_type i = 0; i < len; i++)
 			A.construct(arr + i, val);
 	};
-	template <class InputIterator>
-	vector(InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type()) : A(alloc), len(0), cap(0) {
+
+	template<class InputIterator>
+	vector(InputIterator first, InputIterator last, const allocator_type &alloc = allocator_type()) : A(alloc), len(0),
+																									  cap(0) {
 		for (; first + len != last; ++cap, ++len);
 		arr = A.allocate(cap);
 		for (size_type i = 0; first + i != last; ++i)
 			A.construct(&arr[i], *(first + i));
 	};
-	vector(const vector& x) : A(x.A), len(x.len), cap(x.cap) {
+
+	vector(const vector &x) : A(x.A), len(x.len), cap(x.cap) {
 		arr = A.allocate(cap);
 		for (size_type i = 0; i < len; i++)
 			A.construct(&arr[i], x.arr[i]);
@@ -307,19 +386,19 @@ public:
 	/* Destructor */
 
 	~vector() {
-		for (size_type i = 0; i < len; i++)
-			A.destroy(&arr[i]);
+		for (size_type i = len; i > 0; i--)
+			A.destroy(arr + i - 1);
 		A.deallocate(arr, cap);
 	}
 
 	/* Non-member function overloads */
 
-	template <class V, class alloc>
-	friend void swap(vector<V,alloc> &x, vector<V,alloc> &y) { x.swap(y); };
+	template<class V, class alloc>
+	friend void swap(vector<V, alloc> &x, vector<V, alloc> &y) { x.swap(y); };
 
 	/* relational operators */
 
-	friend bool operator==(	const vector<T,Alloc> &lhs,	const vector<T,Alloc> &rhs) {
+	friend bool operator==(const vector<T, Alloc> &lhs, const vector<T, Alloc> &rhs) {
 		if (lhs.len == rhs.len) {
 			for (size_type i = 0; i < lhs.len; i++) {
 				if (lhs.arr[i] != rhs.arr[i])
@@ -329,8 +408,10 @@ public:
 		}
 		return false;
 	}
-	friend bool operator!=(	const vector<T,Alloc> &lhs,	const vector<T,Alloc> &rhs) { return !(lhs == rhs); }
-	friend bool operator<(	const vector<T,Alloc> &lhs,	const vector<T,Alloc> &rhs) {
+
+	friend bool operator!=(const vector<T, Alloc> &lhs, const vector<T, Alloc> &rhs) { return !(lhs == rhs); }
+
+	friend bool operator<(const vector<T, Alloc> &lhs, const vector<T, Alloc> &rhs) {
 		if (lhs.len < rhs.len)
 			return true;
 		for (size_type i = 0; i < lhs.len; i++) {
@@ -341,9 +422,16 @@ public:
 		}
 		return false;
 	}
-	friend bool operator<=(	const vector<T,Alloc> &lhs,	const vector<T,Alloc> &rhs) { return !(rhs < lhs); }
-	friend bool operator>(	const vector<T,Alloc> &lhs,	const vector<T,Alloc> &rhs) { return rhs < lhs; }
-	friend bool operator>=(	const vector<T,Alloc> &lhs,	const vector<T,Alloc> &rhs) { return !(lhs < rhs); } // Relational operators for vector (function template )
+
+	friend bool operator<=(const vector<T, Alloc> &lhs, const vector<T, Alloc> &rhs) { return !(rhs < lhs); }
+
+	friend bool operator>(const vector<T, Alloc> &lhs, const vector<T, Alloc> &rhs) { return rhs < lhs; }
+
+	friend bool operator>=(const vector<T, Alloc> &lhs, const vector<T, Alloc> &rhs) {
+		return !(lhs < rhs);
+	} // Relational operators for vector (function template )
 };
+
+}
 
 #endif
--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ public:
 	int *a;
 
 	A() : i(0), a(new int[5]()) { std::cout << "constructor\n"; };
-	A(int i) : i(i), a(new int[5]()) { std::cout << i << " int constructor\n"; };
+	explicit A(int i) : i(i), a(new int[5]()) { std::cout << i << " int constructor\n"; };
 	A(A const &r) : i(r.i), a(new int[5]()) { std::cout << r.i << " copy constructor\n"; };
 	~A() { std::cout << i << " destructor\n"; delete [] a; }
 	A &operator=(A const &other) { std::cout << other.i << " assign operator\n"; if (this == &other) return *this; this->i = other.i; return *this; };
@@ -34,32 +34,12 @@ bool func3(int i) {
 	return i <= 11;
 }
 
+int i = 0;
+
 int gen() {
-	return rand() % 100;
+	return i++;
 }
 
 int main() {
-	A arr[5] = {1, 2, 3, 4, 5};
-	std::cout << "\n";
-	A b(0);
-	std::cout << "\n";
-	A c(6);
-	std::cout << "\n";
-	A d(7);
-	std::cout << "\n";
-	std::vector<A> a(arr, arr + 5);
-	std::cout << "\n";
 
-	a.insert(a.begin() + 1, 1, c);
-	std::cout << "\n";
-	a.insert(a.end(), 2, d);
-	std::cout << "\n";
-	a.insert(a.begin(), 1, c);
-	std::cout << "\n";
-	a.insert(a.end() - 5, 11, d);
-	std::cout << "\n";
-//	a.insert(a.end(), d);
-//	std::cout << "\n";
-	std::for_each(a.begin(), a.end(), func1)(b);
-	std::cout << "\nsize: " << a.size() << "\ncapacity: " << a.capacity() << std::endl;
 }
